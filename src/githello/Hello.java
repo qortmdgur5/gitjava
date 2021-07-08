36142ec849bf5bdea4aa87f869e913adc368dc0f
baeksh <dnfg12@naver.com>
2021-07-08 14:21:53 +0900

    충돌테스트!!!

--- src/githello/Hello.java
+++ src/githello/Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		
+		System.out.println("월요일부터는 비대면 수업입니다.");
 		System.out.println("안녕하세요 홍길동 님");
-		System.out.println("만나서 반갑습니다.");
+		System.out.println("hello2");
 		System.out.println("오늘은 목요일 입니다.");
 	}
 
